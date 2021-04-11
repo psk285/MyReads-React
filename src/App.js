59ae6654a5001286504a7f5a2e5eca2f135a0567
Pratik Kamath <psk285@nyu.edu>
2021-04-11 16:27:34 -0400

    BookShelf component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import * as BooksAPI from './BooksAPI'
 import "./App.css";
-import Book from "./Components/Book";
+import BookShelf from "./Components/BookShelf";
 import { getAll } from "./BooksAPI";
 
 class BooksApp extends React.Component {
@@ -13,8 +13,24 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
+    read: [],
+    wantToRead: [],
+    currentlyReading: [],
   };
-
+  async componentDidMount() {
+    let allBooks = await getAll();
+    console.log(allBooks);
+    let initCurrentlyReading = allBooks.filter(
+      (book) => book.shelf === "currentlyReading"
+    );
+    let initWantToRead = allBooks.filter((book) => book.shelf === "wantToRead");
+    let initRead = allBooks.filter((book) => book.shelf === "read");
+    this.setState({
+      read: initRead,
+      currentlyReading: initCurrentlyReading,
+      wantToRead: initWantToRead,
+    });
+  }
   render() {
     return (
       <div className="app">
@@ -50,100 +66,15 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <Book
-                        sty={{
-                          width: 128,
-                          height: 193,
-                          backgroundImage:
-                            'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                        }}
-                        tit="To Kill a Mockingbird"
-                        auth="Harper Lee"
-                      />
-                      <Book
-                        sty={{
-                          width: 128,
-                          height: 188,
-                          backgroundImage:
-                            'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
-                        }}
-                        tit="Ender's Game"
-                        auth="Orson Scott Card"
-                      />
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <Book
-                        sty={{
-                          width: 128,
-                          height: 193,
-                          backgroundImage:
-                            'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
-                        }}
-                        tit="1776"
-                        auth="David McCullough"
-                      />
-
-                      <Book
-                        sty={{
-                          width: 128,
-                          height: 192,
-                          backgroundImage:
-                            'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")',
-                        }}
-                        tit="Harry Potter and the Sorcerer's Stone"
-                        auth="J.K. Rowling"
-                      />
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <Book
-                        sty={{
-                          width: 128,
-                          height: 192,
-                          backgroundImage:
-                            'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")',
-                        }}
-                        tit="The Hobbit"
-                        auth="J.R.R. Tolkien"
-                      />
-
-                      <Book
-                        sty={{
-                          width: 128,
-                          height: 174,
-                          backgroundImage:
-                            'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")',
-                        }}
-                        tit="Oh, the Places You'll Go!"
-                        auth="Seuss"
-                      />
-
-                      <Book
-                        sty={{
-                          width: 128,
-                          height: 192,
-                          backgroundImage:
-                            'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")',
-                        }}
-                        tit="The Adventures of Tom Sawyer"
-                        auth="Mark Twain"
-                      />
-                    </ol>
-                  </div>
-                </div>
+                <BookShelf
+                  shelfTitle="Currently Reading"
+                  bookDetails={this.state.currentlyReading}
+                />
+                <BookShelf
+                  shelfTitle="Want to Read"
+                  bookDetails={this.state.wantToRead}
+                />
+                <BookShelf shelfTitle="Read" bookDetails={this.state.read} />
               </div>
             </div>
             <div className="open-search">
