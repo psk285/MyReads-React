762dee52d61e404c972f6b8e9ccec9d4c550b2ff
Pratik Kamath <psk285@nyu.edu>
2021-04-11 21:25:48 -0400

    Moving between shelfs completed

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Install react-router-dom
+npm install --save react-router-dom
+
 Steps to run the project:
 npm install
 npm start
--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 // import * as BooksAPI from './BooksAPI'
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import BookShelf from "./Components/BookShelf";
-import { getAll } from "./BooksAPI";
+import { getAll, update } from "./BooksAPI";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-    read: [],
-    wantToRead: [],
-    currentlyReading: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      /**
+       * TODO: Instead of using this state variable to keep track of which page
+       * we're on, use the URL in the browser's address bar. This will ensure that
+       * users can use the browser's back and forward buttons to navigate between
+       * pages, as well as provide a good URL they can bookmark and share.
+       */
+      showSearchPage: false,
+      books: [],
+    };
+  }
+
+  moveBookToShelf = (book, shelf) => {
+    let prevShelf = book.shelf;
+    let nextShelf = shelf;
+    if (prevShelf === nextShelf) return;
+    update(book, nextShelf);
+    console.log(book, shelf);
+    let actualBook = this.state.books.filter((bo) => bo.id === book)[0];
+    console.log(actualBook);
+    actualBook.shelf = nextShelf;
+    //console.log(actualBook);
+    let allBooksExceptCurrentBook = this.state.books.filter(
+      (b) => b.id !== book
+    );
+    let newUpdatedBooks = [...allBooksExceptCurrentBook, actualBook];
+    this.setState({ books: newUpdatedBooks });
   };
   async componentDidMount() {
     let allBooks = await getAll();
-    console.log(allBooks);
-    let initCurrentlyReading = allBooks.filter(
-      (book) => book.shelf === "currentlyReading"
-    );
-    let initWantToRead = allBooks.filter((book) => book.shelf === "wantToRead");
-    let initRead = allBooks.filter((book) => book.shelf === "read");
     this.setState({
-      read: initRead,
-      currentlyReading: initCurrentlyReading,
-      wantToRead: initWantToRead,
+      books: allBooks,
     });
   }
   render() {
@@ -68,13 +78,25 @@ class BooksApp extends React.Component {
               <div>
                 <BookShelf
                   shelfTitle="Currently Reading"
-                  bookDetails={this.state.currentlyReading}
+                  shelfTitleProps="currentlyReading"
+                  bookDetails={this.state.books}
+                  moveBookToShelf={this.moveBookToShelf}
+                  key={this.state.books.id}
                 />
                 <BookShelf
                   shelfTitle="Want to Read"
-                  bookDetails={this.state.wantToRead}
+                  shelfTitleProps="wantToRead"
+                  bookDetails={this.state.books}
+                  moveBookToShelf={this.moveBookToShelf}
+                  key={this.state.books.id}
                 />
-                <BookShelf shelfTitle="Read" bookDetails={this.state.read} />
+                <BookShelf
+                  shelfTitle="Read"
+                  shelfTitleProps="read"
+                  bookDetails={this.state.books}
+                  moveBookToShelf={this.moveBookToShelf}
+                  key={this.state.books.id}
+                />
               </div>
             </div>
             <div className="open-search">
@@ -84,6 +106,104 @@ class BooksApp extends React.Component {
             </div>
           </div>
         )}
+      </div>
+    );
+  }
+}
+
+class BookShelf extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      booksToShow: [],
+    };
+  }
+
+  async componentDidMount() {
+    let filtBooks = this.props.bookDetails.filter(
+      (bookDetail) => bookDetail.shelf === this.props.shelfTitleProps
+    );
+    this.setState({ booksToShow: filtBooks });
+  }
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.bookDetails
+              .filter(
+                (bookDetail) => bookDetail.shelf === this.props.shelfTitleProps
+              )
+              .map((bookDetail, index) => (
+                <Book
+                  idBook={bookDetail.id}
+                  tit={bookDetail.title}
+                  key={bookDetail.id}
+                  sty={{
+                    width: 148,
+                    height: 174,
+                    backgroundImage:
+                      bookDetail.imageLinks &&
+                      `url('${bookDetail.imageLinks.thumbnail}')`,
+                  }}
+                  shelf={bookDetail.shelf}
+                  auth={bookDetail.authors}
+                  moveBookToShelf={this.props.moveBookToShelf}
+                />
+              ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Book extends Component {
+  render() {
+    return (
+      <li>
+        <div className="book" id={this.props.tit}>
+          <div className="book-top">
+            <div className="book-cover" style={this.props.sty} />
+            <BookShelfChanger
+              changerKey={this.props.idBook}
+              shelf={this.props.shelf}
+              moveBookToShelf={this.props.moveBookToShelf}
+            />
+          </div>
+          <div className="book-title">{this.props.tit}</div>
+          <div className="book-authors">{this.props.auth}</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+class BookShelfChanger extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shelf: this.props.shelf,
+    };
+    this.eventHandlerForChange = this.eventHandlerForChange.bind(this);
+  }
+  eventHandlerForChange(e) {
+    this.setState({ shelf: e.target.value });
+    this.props.moveBookToShelf(this.props.changerKey, e.target.value);
+  }
+  render() {
+    return (
+      <div className="book-shelf-changer" key={this.props.changerKey}>
+        <select value={this.state.shelf} onChange={this.eventHandlerForChange}>
+          <option value="move" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
       </div>
     );
   }
